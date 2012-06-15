4cf4528a2c39e005bc8d902f872250f0a277bc59
kanechika <kanechika7@gmail.com>
2012-06-15 18:41:18 +0900

    add r8/controller

--- r8/controller.rb
+++ r8/controller.rb
@@ -1,13 +1,41 @@
 # coding: UTF-8
 # 
-# How To
+# SETTING
 #
-# 1. Setting
+# $ vim app/controllers/applications_controller.rb
+# class ApplicationsController
+#   include Strut::Controller
+#   include R8::Controller
+#
 # $ vim app/controllers/items_controller.rb
 # class ItemsController
-#   include R8::Controller
+#   strut_controller Item
+#   r8_controller Item
+#
+#   # @i18n_opts
+#   def index_i18n
+#     @i18n_opts = { items_name: 'AAAAAAAAAAAA' }
+#   end
+#   def show_i18n
+#     @i18n_opts = { item_name: 'AAAAAAAAAAA' }
+#   end
 #
 module R8
   module Controller
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      include R8::Controller::Filter
+      def r8_controller clazz, options={}
+        table_name = clazz.to_s.tableize.gsub("/","_")
+        file_name = clazz.to_s.underscore.gsub("/","_")
+        actions = R8::Model::Holder.new options
+
+        # define_before_filter
+        i18n_controller_filter clazz, table_name, file_name, actions
+        
+      end
+    end
+
   end
 end
