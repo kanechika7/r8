b11b7379cc51579036d068a913c3f7df82b40b0c
kanechika <kanechika7@gmail.com>
2012-07-02 23:19:31 +0900

    add single_valid_message

---
+++ r8/model/single_valid_message.rb
@@ -0,0 +1,38 @@
+# coding: UTF-8
+
+#
+# SETTING
+# 
+#   class Item
+#     include R8::Model::SingleValidMessage
+#
+#
+# HOW TO
+#
+#   = @item.single_valid_message(:name)  
+#
+
+module R8
+  module Model
+    module SingleValidMessage
+      extend ActiveSupport::Concern
+
+      module InstanceMethods
+
+        def single_valid_message field
+          return "" if errors.empty? or errors[field].empty?
+          unsco_clazz = self.class.to_s.underscore
+          messages = errors[field].map{|msg| content_tag(:li, I18n.t("activerecord.attributes.#{unsco_clazz}.#{field}")) }.uniq.join
+          html = <<-HTML
+            <div class='error_explanation'>
+              <ul>#{messages}</ul>
+            </div>
+          HTML
+          html.html_safe
+        end 
+
+      end
+
+    end
+  end
+end
