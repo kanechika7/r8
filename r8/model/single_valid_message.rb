f76f728c0049ee68c5af7ce5b0a131aae8ff0a7c
kanechika7 <kanechika7@gmail.com>
2012-12-09 18:19:15 +0900

    single_valid_message 修正

--- r8/model/single_valid_message.rb
+++ r8/model/single_valid_message.rb
@@ -22,7 +22,8 @@ module R8
         def single_valid_message field
           return "" if errors.empty? or errors[field].empty?
           unsco_clazz = self.class.to_s.underscore
-          messages = errors[field].map{|msg| "<li>"+I18n.t("mongoid.attributes.#{unsco_clazz}.#{field}")+msg+"</li>" }.uniq.join
+          #messages = errors[field].map{|msg| "<li>"+I18n.t("mongoid.attributes.#{unsco_clazz}.#{field}")+msg+"</li>" }.uniq.join
+          messages = errors[field].map{|msg| "<li>"+self.class.human_attribute_name(field.to_sym)+msg+"</li>" }.uniq.join
           html = <<-HTML
             <div class='error_explanation'>
               <ul>#{messages}</ul>
